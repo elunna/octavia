22ee8519c51f70152e3f27022f9c7d7636796b3e
Erik Lunna <eslunna@gmail.com>
2016-11-05 04:07:02 -0400

    Gave up on the embedded youtube-dl - just cannot get the libmp3lame library to work with ffmpeg.

--- ytem.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python2
-
-import youtube_dl
-
-with youtube_dl.YoutubeDL({}) as ydl:
-    ydl.download(['http://www.youtube.com/watch?v=BaW_jenozKc'])
-
-def download(url):
-    pass
