c2bd03f431a95d6208325e0cf212921b008b8927
PythonIsMagic <eslunna@gmail.com>
2016-11-30 17:41:38 -0500

    Added comment to setup.sh.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Use this to create a globally usable script so you can download to any directory from any directory.
+# Make sure you have a ~/bin folder.
+
 BINDIR=$HOME/bin
 SCRIPT=octavia.py
 
